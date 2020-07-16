9bbc183b1b0693df0489e2f5d2c1b63153f88cdf
Haitao Mao <61724600+ZhangXueYouo@users.noreply.github.com>
2020-07-16 10:15:58 -0700

    Update CuriousBunny.java

--- CuriousBunny.java
+++ CuriousBunny.java
@@ -1,6 +1,6 @@
 /*Author: Paola Flores ID T00651883*/
 
-public class CuriousBunny extends Animal
+public class CuriousBunny extends Animal implements Teleporter
 {
      public CuriousBunny()
      {
@@ -23,5 +23,27 @@ public class CuriousBunny extends Animal
                System.out.println(super.name + " is hopping");
           }
      }//end of hop()
+     
+     public void teleport (int x, int y)
+     {
+       if( x > HALF || x < 0 )
+       {
+         System.out.println(this.getName()+ " cant teleport,value x is out of range [0,500]");
+         return;
+       }
+       
+       if( y > HEIGHT || y < 0 )
+       {
+         System.out.println(this.getName() + " cant teleport, value y is out of range [0,1000]");
+       }
+       this.x = x;
+       this.y = y;
+       System.out.println(getPosition());
+     }
+     
+     public String getPosition() 
+     {
+       return this.getName() + "'s position is:(" + x + "," + y +")";
+     }
      
 }//end of class
