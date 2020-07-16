72fdd6b762ecbdc17629443bfb0f67fc32aa502b
Haitao Mao <61724600+ZhangXueYouo@users.noreply.github.com>
2020-07-16 10:56:45 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,93 +1,147 @@
-/*Author: Paola Flores T0065188*/
+//Hai tao Mao T00646865
 
-public abstract class Animal
+public abstract class Animal 
 {
-     protected String name, primaryColor;
-     protected int health;
-     protected static int animalCount;
-    
-     //world map, 
-     public static final int WIDTH = 1000;
-     public static final int HEIGHT = 1000;
-     public static final int HALF = 500; //half width
-     //initial position (x,y)=(0,0)
-     public static int x = 0;
-     public static int y = 0;
-     
-     public interface Teleporter 
-     {
+  
+   protected String name;
+   protected String primaryColor;
+   protected static int count = 0;
+   protected int health = 3;
+   protected boolean isAlive = true;
+   
+   //Killpoint
+   protected static int killPoint = 0; 
+   
+   //world map, 
+   public static final int WIDTH = 1000;
+   public static final int HEIGHT = 1000;
+   public static final int HALF = 500; //half width
+   
+   
+   //initial position (x,y)=(0,0)
+   public static int x = 0;
+   public static int y = 0;
+   
+   
+   public interface Teleporter 
+   {
        public void teleport (int x, int y);
        public String getPosition() ;
-     }
+   }
+   
+   /*
+   public interface  Attacker 
+   {
+       public void attack (String name)
+       {
+           if(isAlive)
+           {
+            if 
+             
+           }
+           else
+           {
+               System.out.println("Attacker already dead, cant attack!");
+           }
+       }
 
-     public Animal()
-     {
-          this.name="unknown";
-          this.primaryColor="unknown";
-          animalCount++;
-          this.health = 3;
-     }//end of Animal()
-     
-     public Animal (String name, String primaryColor)
-     {
-          this.name=name;
-          this.primaryColor=primaryColor;
-          animalCount++;
-          this.health = 3;
-     }//end of Animal()
-     
-     public String getName()
-     {
-          return name;
-     }//end of getName()
-     
-     public void setName(String newName)
-     {
-          this.name=newName;
-     }//end of setName()
-     
-     public String getColor()
-     {
-          return primaryColor;
-     }//end of getColor()
-     
-     public void setColor(String newColor)
-     {
-          this.primaryColor=newColor;
-     }//end of setColor()
-     
-     public String toString()
-     {
-          return name +" is "+primaryColor;
-     }//end of toString()
-     
-     public int getHealth()
-     {
-          return health;
-     }//end of getHealth()
-     
-     public static void getCount()
-     {
-          System.out.println(animalCount);
-     }//end of getCount()
-     
-     public void hit()
-     {
-          if (this.health == 0) 
-          {
-               System.out.println("Animal is dead");
-          }
-          else
-          {
-               if(this.health==1)
-               {
-                    animalCount--;
-               }
-               {
-                    this.health--;
-                    System.out.println("Oh no! " + this.name + " got hit!! It has " +
-                                       this.health + " lives left");
-               }
-          }
-     }//end of hit()     
-}//end of abstract class 
+  
+       public int getKillCount() 
+       {
+           return killPoint;
+       }
+   
+   }
+   */
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   public Animal(String name, String primaryColor) 
+   {
+       super();
+       count ++;
+       this.name = name;
+       this.primaryColor = primaryColor;
+      
+   }
+  
+   // set & get name
+   public String getName() 
+   {
+       return name;
+   }
+
+   public void setName(String name) 
+   {
+       this.name = name;
+   }
+  
+   // set & get primaryColor
+   public String getPrimaryColor() 
+   {
+
+       return primaryColor;
+   }
+
+   public void setPrimaryColor(String primaryColor) 
+   {
+       this.primaryColor = primaryColor;
+   }
+   
+   // s%g Health
+   public int getHealth() 
+   {
+       return health;
+   }
+
+   public void setHealth(int health) 
+   {
+       this.health = health;
+   }
+   
+   
+   // hit & isAlive status
+   public void hit() 
+   {
+       if (isAlive) 
+       {
+           System.out.println("Pikachu used THUNDER SHOCK!");
+           health--;
+           
+           
+       if (!isAlive()) 
+       {
+           System.out.println("Animal is already fainted, can't hit it again!");
+           return;
+       }
+       }
+       if (this.health == 0) 
+       {
+           this.isAlive = false;
+           count --;
+           System.out.println( "*************************************************");
+           System.out.println(this.name + " fainted, Pikachu Gained 115 EXP.Points !!");
+           System.out.println( "*************************************************");
+       }
+   }
+   
+   public String toString()
+   {
+       return "Name:" + name +
+         "\nColor:"  + primaryColor +
+         "\nhealth:" + health;
+   }
+   
+   public boolean isAlive() 
+   {
+       return isAlive;
+   }
+}
